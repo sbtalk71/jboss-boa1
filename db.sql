cfc1bd2ef0ac05dab5d0bad9a881b2e89f7b0e92
sbtalk71 <sbtalk@yahoo.com>
2025-04-22 16:43:03 +0530

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,14 @@
+create database empdb;
+use empdb;
+drop table MYEMP;
+create table MYEMP (empid int primary key,name varchar(20),salary double);
+
+insert into MYEMP (empid,name,salary) values (1,'Ankur',20000);
+insert into MYEMP (empid,name,salary) values (2,'Ranbir',20000);
+insert into MYEMP (empid,name,salary) values (3,'Rahul',20000);
+insert into MYEMP (empid,name,salary) values (4,'Ramu',20000);
+insert into MYEMP (empid,name,salary) values (5,'Baker',20000);
+insert into MYEMP (empid,name,salary) values (6,'Bajrang',20000);
+insert into MYEMP (empid,name,salary) values (7,'Hamid',20000);
+insert into MYEMP (empid,name,salary) values (8,'Hanuman',20000);
+insert into MYEMP (empid,name,salary) values (9,'Tapas',20000);
